c840fd4ef470e22cc75e48c79f2b3c30db796f91
UTILISATEUR <UTILISATEUR@172.16.237.70>
2018-05-02 11:55:10 +0200

    piece maison et info

---
+++ html/informations.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="ISO-8859-1">
+		<link rel="stylesheet" href="../css/style.css">	
+		<title>Mes informations</title>
+	</head>
+	
+	<body>
+
+		<header>
+			<?php
+                require("en_tete_connexion.php");
+	       ?>
+		</header>
+
+		<article>
+		
+			<h1>Mes informations</h1>
+		
+		
+			<input type="submit" value="Modifier mes informations" />
+		
+		</article>
+
+	</body>
+</html>
