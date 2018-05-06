57d4e1f2891e5c7c179f9eef5b8287ab9250881d
Ulrich971 <ulrich.firpion@isep.fr>
2018-05-06 20:24:52 +0200

    modif pour crypter mdp dans la bdd lors de l'inscription

--- html/traitement2.php
+++ html/traitement2.php
@@ -4,15 +4,10 @@ if (!empty($_POST)) {   // les données du formulaires ont été complétées, o
     
     
     $req = $pdo ->prepare('INSERT INTO utilisateur(nom,mail,motdepasse,numerodetelephone) VALUES (?,?,?,?)');
-    $req->execute(array($_POST['username'],$_POST['mail'],$_POST['password'],$_POST['numero_de_tel']));
-    /*$req->execute([$_POST['mail']]);
-    /*$req->execute([$_POST['password']]);
-    $req->execute([$_POST['numero_de_tel']]);  */
+    $pass = $_POST['password'];
+    $hash = password_hash($pass,PASSWORD_BCRYPT);
+    $req->execute(array($_POST['username'],$_POST['mail'],$hash,$_POST['numero_de_tel'])); 
     }
     header('location: page_de_connexion.php');
-   /* else(){
-        
-    }*/
   
-
 ?>
