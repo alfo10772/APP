9e339c10998123f7564337f7ead1b5c1d2822154
Naomi <naomicarlotti@yahoo.fr>
2018-05-10 16:26:26 +0200

    création traitement suppression composant, lien avec bdd

---
+++ html/traitement_suppression_composant.php
@@ -0,0 +1,20 @@
+<?php
+
+$bdd = NULL;
+
+try
+{
+    $bdd = new PDO('mysql:host=localhost;dbname=bdd_a;charset=utf8','root','',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)); // A modifier lors de l'hebergement
+}
+catch (Exception $e)
+{
+    die('Erreur :' . $e->getMessage());
+}
+
+$maison = $_POST['maison'];
+$piece = $_POST['piece'];
+$composant = $_POST['composant'];
+
+
+$req = $bdd->prepare('DELETE FROM composant WHERE(nom=composant)');
+?>
