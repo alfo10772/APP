a2e93bfe7ffb468d2203c2d950ffab960cd6eab6
Naomi <naomicarlotti@yahoo.fr>
2018-05-15 14:10:57 +0200

    Ajout footer.php plus modif dans ts les fichiers php

--- html/suppression_composant.php
+++ html/suppression_composant.php
@@ -72,34 +72,9 @@
 		</form>
 	</article>
 	<footer>
-		<p class="bordure1">
-			<a href="faq.html">
-				<strong>
-					FAQ
-				</strong>
-			</a>
-		</p>
-		<p>
-			<a href="condition_d'utilisation.html">
-				Condition g&eacute;n&eacute;rales d'utilisation
-			</a>
-		</p>
-		<p>
-			<a href="mentions_legales.html">
-				Mentions l&eacute;gales
-			</a>
-		</p>
-		<div>
-			Date et heure
-			<div id="afficherheure">
-			</div>
-			<script type="text/javascript">
-			setInterval(function(){
-    		document.getElementById('afficherheure').innerHTML = new Date().toLocaleTimeString();
-			}, 1000);
-			</script>
-		</div>
-		</div>
+			<?php
+                require("footer.php");
+            ?>
 	</footer>
 </body>
 </html>
