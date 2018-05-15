415d5d5a82aa2004ce90bdad0defc109a37d6005
alfo10772 <albane.fortier@isep.fr>
2018-05-15 14:35:46 +0200

    menu admin

--- html/menu_admin.php
+++ html/menu_admin.php
@@ -4,7 +4,7 @@ session_start()
 
 <div class="menu-vertical">
 		<p>
-			<img src="../images/LogoHabilis.png" alt="Logo Habilis" width="150">
+			<img src="../images/Logo4.png" alt="Logo Habilis" width="150">
 		</p>
 		<div class="menu"><a href="#">Accueil</a></div>
 		<div class="menu"><a href="#">Clients</a></div>
