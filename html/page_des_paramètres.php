4fbf0804570edf8efdb1d88ab63e7b9e0e45eeb7
Victor Charbonnieras <victor.charbonnieras@isep.fr>
2018-05-16 10:13:34 +0200

    up

--- html/page_des_paramètres.php
+++ html/page_des_paramètres.php
@@ -89,33 +89,9 @@
 	
 
     <footer>
-		<p class="bordure1">
-			<a href="faq.html">
-				<strong>
-					FAQ
-				</strong>
-			</a>
-		</p>
-		<p>
-			<a href="condition_d'utilisation.html">
-				Conditions g&eacute;n&eacute;rales d'utilisation
-			</a>
-		</p>
-		<p>
-			<a href="mentions_legales.html">
-				Mentions l&eacute;gales
-			</a>
-		</p>
-		<div>
-			Date et heure
-			<div id="afficherheure">
-			</div>
-			<script type="text/javascript">
-			setInterval(function(){
-    		document.getElementById('afficherheure').innerHTML = new Date().toLocaleTimeString();
-			}, 1000);
-			</script>
-		</div>
+			<?php
+                require("footer.php");
+            ?>
 	</footer>
 </body>
 
