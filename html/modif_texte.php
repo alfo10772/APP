b6cd2bc0515d6b7f860afb227de74d7dc27416a8
alfo10772 <albane.fortier@isep.fr>
2018-05-17 22:51:05 +0200

    modif page modif texte + css

--- html/modif_texte.php
+++ html/modif_texte.php
@@ -1,84 +1,63 @@
 <!DOCTYPE html>
- <html>
-<head>
- <meta charset="utf-8">
+<html>
+	<head>
+ 		<meta charset="utf-8">
+   
+    	<link rel="stylesheet" href="../css/style.css">		<!--  lien vers style -->
+
+ 		<title>Modifier les textes</title>				<!--  titre de la page -->
+	</head>
  
+ 	<body>	
 
-  
-    <link rel="stylesheet" href="../css/style.css">		<!--  lien vers style -->
-
- 	<title>Modifier les textes</title>				<!--  titre de la page -->
-</head>
- <body>
-
+ 		<div id="article2">
 	
-		
-
- <header>
- 	<?php
-         require("en_tete_connexion.php");
-	?>
- </header>
-  	
-  	
-  	
- <article>
- 
-		<h1>Modification des textes</h1>
+			<?php 
+	          require("menu_admin.php");
+	        ?>
 			
-		<div style="float:left">
-				<a href="tableau_de_bord.php">		
-					<input type="submit" id="retour" value="Retour &agrave; la page d'accueil" />
-				</a>
-		</div> 
+			<?php 
+       		   include('../modele/config_init.php');
+       	    ?>
+  	
+  			<div class="contenu">
  
+				<h1>Modification des textes</h1>
+			
+				<br/>	
  
- <br/>
- <br/>
- <br/>
- <br/>
- <br/>
+ 				<div id=conteneurtexte>
  
- 	<div id=conteneurtexte>
- 
- 		<form  method="post" action=".php">
- 			<div id="formulaire1">
- 				<input type="text" placeholder="Modifier le texte de pr�sentation de l'entreprise" style="width:500px; height:125px">
- 				<br/>
- 				<br/>
- 				<input type="submit" id="supprimer" value="Enregistrer" />
+ 					<form  method="post" action=".php">
+ 						<div id="formulaire1">
+ 							<input type="admin" placeholder="Modifier le texte de pr&eacute;sentation de l'entreprise" >
+ 							<br/>
+ 							<br/>
+ 							<input type="submit2" value="Enregistrer" />
  	
- 			</div>
- 		</form>
- 	<br/>
- 	<br/>
- 	<br/>
- 	<br/>
+ 						</div>
+ 					</form>
+ 					
+ 					<br/>
+ 					<br/>
 
- 		<form  method="post" action=".php">
- 			<div id="formulaire1">
- 				<input type="text" placeholder="Modifier les conditions générales d'utilisation" style="width:500px; height:125px">
- 				<br/>
- 				<br/>
- 				<input type="submit" value="Enregistrer" />
- 			</div>
- 		</form>
- 	  <br/>
- 	  <br/>
- 	  <br/>
+ 					<form  method="post" action=".php">
+ 						<div id="formulaire1">
+ 							<input type="admin" placeholder="Modifier les conditions générales d'utilisation" >
+ 							<br/>
+ 							<br/>
+ 							<input type="submit2" value="Enregistrer" />
+ 						</div>
+ 					</form>
+ 					
+ 	  				<br/>
+ 	 				<br/>
+ 	 				<br/>
  	  
- 	  </div>
+ 	 			 </div>
  	 
-  	</article>
- 	
- 	
- 	
- 	<footer>
-		<?php
-         require("footer.php");
-	    ?>
- 	 </footer>
- 	 
+ 	 		</div>
+  	 
   </body>
 </html>
 	
