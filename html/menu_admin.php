ba114dbf205de8c5d7b5aee4f0cca1bca3ba0bc0
alfo10772 <albane.fortier@isep.fr>
2018-05-22 12:39:47 +0200

    correction benoit page admin

--- html/menu_admin.php
+++ html/menu_admin.php
@@ -11,7 +11,6 @@ session_start()
 		<div class="menu"><a href="admin_message.php">Messages</a></div>
 		<div class="menu"><a href="modif_texte.php">Modifier les textes</a></div>
 		<div class="menu"><a href="modif_article.php">Modifier les articles</a></div>
-		<div class="menu"><a href="modif_CGU.php">Modifier les conditions d'utilisations</a></div>
 		
 </div>
 
