7e3be35f72031ac260ad87f63d5b15bbed870ea1
Naomi <naomicarlotti@yahoo.fr>
2018-05-22 16:51:35 +0200

    Modif affichage

--- html/notification.php
+++ html/notification.php
@@ -20,7 +20,7 @@
 		<?php 
        		include('../modele/config_init.php');
        	
-       		$not = $bdd->query('SELECT * FROM notification');
+       		$not = $bdd->query('SELECT * FROM notification ORDER BY IDnotification DESC');
        		?>
        		  
        		    <table id="notification">
