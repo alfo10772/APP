218aff7ce5cecf25dd902edeae5c7674c78719ee
Ulrich971 <ulrich.firpion@isep.fr>
2018-05-23 11:04:48 +0200

    Ajout js sur l'inscription pour la comparaison des mdp

--- html/traitement.php
+++ html/traitement.php
@@ -7,19 +7,19 @@ if (!empty($_POST)) {   // les données du formulaires ont été complétées, o
     $req = $pdo ->prepare('SELECT IDutilisateur,motdepasse FROM utilisateur WHERE mail =? ');
     $req->execute([$_POST['mail']]);
     $user = $req->fetch(PDO::FETCH_NUM);  // on r�cup�re le premier element dans req
-    print_r($user);
-    print_r($_POST);
-    var_dump (password_verify($_POST['password'],$user[1]));
+   // print_r($user);
+   /* print_r($_POST);
+    var_dump (password_verify($_POST['password'],$user[1]));*/
       
    if (!empty($user)){
        if(password_verify($_POST['password'],$user[1])){
     	session_start();
     	$_SESSION['mail']= $_POST['mail'];
     	$_SESSION['ID']= $user[0];
-       // header('location: tableau_de_bord.php');
+       header('location: tableau_de_bord.php');
    }
    else{
-       //header('location: page_de_connexion.php');
+      header('location: page_de_connexion.php');
    }
    }
 }
