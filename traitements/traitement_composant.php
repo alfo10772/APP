7a225147deffa0832d3f4ee6e13b0667d8c28405
Naomi <naomicarlotti@yahoo.fr>
2018-05-23 12:02:27 +0200

    ajout lien notfs => ajout composant, suppr maison

--- traitements/traitement_composant.php
+++ traitements/traitement_composant.php
@@ -16,8 +16,11 @@ $piece = $_POST['piece'];
 $requetepiece = $bdd->query("SELECT IDpiece FROM piece WHERE nom='". $piece ."' ;");
 $piece = $requetepiece ->fetch();
 $piece = $piece['IDpiece'];
+$notif=$nom.' a bien &eacute;t&eacute; ajout&eacute;e';
 
 $req = $bdd->prepare('INSERT INTO composant(type, nom, valeurmin, valeurmax, IDpiece) VALUES(:type,:nom,:valeurmin,:valeurmax,:piece)');
+$req2 = $bdd->prepare('INSERT INTO notification(texte) VALUES(:notif)');
 $result = $req->execute(array(':type' => $type, ':nom' => $nom,':valeurmin' => $valeurmin,':valeurmax' => $valeurmax, ':piece' => $piece));
+$result2 = $req2->execute(array(':notif' => $notif));
 header('location: ../html/page_des_composants.php');
 ?>
