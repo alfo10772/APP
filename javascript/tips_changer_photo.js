4c9b392cbda0ff9ed3d1b5ad657da99314dd0303
user <jiaqi.hu@isep.fr>
2018-05-28 11:54:32 +0200

    update

--- javascript/tips_changer_photo.js
+++ javascript/tips_changer_photo.js
@@ -1,5 +1,15 @@
+ //<style>
+//*{ margin:0; padding:0}
+//.box{ height:50px; width:125px; position:relative}
+//.mask{ position:absolute; height:100%; width:100%; top:5; left:0; background:#000; opacity:.3; display:none}
+//.sub{ position:absolute;top:5; left:0; padding:20px; color:#fff; font-size:12px; line-height:2; display:none}
+//.txtShow .mask,.txtShow .sub{ display:block;}
+//</style>
+
+
+//<script>
 window.onload=function(){
-var oDiv=document.getElementById('profil');
+var oDiv=document.getElementById('imgBox');
 
 oDiv.onmouseover=function(){
 this.className='box txtShow';
@@ -10,4 +20,10 @@ this.className='box';
 
 
 }
-
+//</script>
+//<div id="imgBox" class="box">
+//<a href="modif_info.php">
+//<img src="../images/photo.png" alt="Photo profil" width="125"></a>
+				
+    //<div class="mask"></div> 
+    //<p class="sub">Changer la photo</p>
