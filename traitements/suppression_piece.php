74c1f64821efea58c54b878f4fe56b9cb0fe04da
Naomi <naomicarlotti@yahoo.fr>
2018-05-30 11:35:36 +0200

    modif suppr piece et maison (en fonction de l'IDuser)

--- traitements/suppression_piece.php
+++ traitements/suppression_piece.php
@@ -2,7 +2,7 @@
 session_start();
 $bdd = NULL; 
 
-try 
+try
 {
     $bdd = new PDO('mysql:host=localhost;dbname=bdd_a;charset=utf8','root','',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)); // A modifier lors de l'hebergement
 } 
@@ -11,14 +11,14 @@ catch (Exception $e)
     die('Erreur :' . $e->getMessage());
 }
 
-$maison = $_POST['nom_maison'];
+$id=$_SESSION['ID'];
+$idmaison = $_SESSION['maisonselect'];
 $nom = $_POST['nom_piece'];
 $notif=$nom.' a bien &eacute;t&eacute; supprim&eacute;e';
-$id=$_SESSION['ID'];
 
-$req = $bdd ->prepare('DELETE FROM piece WHERE nom = :nom ');
+$req = $bdd ->prepare('DELETE FROM piece WHERE (nom = :nom  AND IDutilisateur = :id AND IDmaison = :idmaison)');
 $req2 = $bdd->prepare('INSERT INTO notification(texte, IDutilisateur) VALUES(:notif, :id)');
-$req-> execute(array(':nom' => $nom));
+$req-> execute(array(':nom' => $nom, ':id' => $id, ':idmaison' => $idmaison));
 $result2 = $req2->execute(array(':notif' => $notif, ':id' => $id));
 header('location: ../html/piece.php');
 
