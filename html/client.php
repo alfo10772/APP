02f8ac0d7c74af9cbd518dc2bfe4b0d761f8a350
Victor Charbonnieras <victor.charbonnieras@gmail.com>
2018-06-05 09:30:02 +0200

    up esthétique

--- html/client.php
+++ html/client.php
@@ -25,7 +25,7 @@
 			
 				<?php 
        					
-       		        $reponse = $bdd->query('SELECT * FROM utilisateur');
+       		        $reponse = $bdd->query('SELECT * FROM utilisateur WHERE type != 2');
        			?>	
        		  
        		    <table>
