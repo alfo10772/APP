3ff52781fd36fd9e7307f63e81229dea2ef58307
Naomi <naomicarlotti@yahoo.fr>
2018-06-08 12:52:31 +0200

    modif gestion => ajout notif = modif bouton ajout user second

--- html/gestion_secondaire.php
+++ html/gestion_secondaire.php
@@ -19,6 +19,7 @@
 		<h1>G&eacute;rer les utilisateurs secondaires</h1>
 		<br>
 		<br>
+		<div id="conteneursecond">
 		<table id="secondaire">
        		     	<tr>
        		       		<th id="nom">Nom</th>
@@ -42,8 +43,11 @@
      					<?php if($donnees['type']==1) {?> Utilisateur secondaire <?php } else{?> Utilisateur principal <?php }?>
      					</td>
      					<td id="changer">
-     						<input type="submit" id="supprimer" value=<?php 
+     					<abbr title="Un utilisateur principal poss&egrave;de des droits que l'utilisateur secondaire n'a pas, comme par exemple les gestion des composants, des pi&egrave;ces, et des maisons (ajouter ou supprimer un &eacute;l&eacute;ment).
+L'utilisateur secondaire peut uniquement voir les donn&eacute;es des composants et effectuer des actions basiques comme allumer les lumi&egrave;res ou ouvrir les volets. ">
+     						<input type="submit" id="seconda" value=<?php 
      						if($donnees['type']==1) {?> "Devenir utilisateur principal" <?php } else{?> "Devenir utilisateur secondaire"<?php }?> >
+     						</abbr>
      						<input type="hidden" name="id" value="<?php echo $donnees['IDutilisateur'];?>">
      						<input type="hidden" name="type" value="<?php echo $donnees['type'];?>">
      					</td>
@@ -53,12 +57,14 @@
                         }
                     ?>
   				</table>
-		<br>
-		<br>
-		<div style="float:right; margin-bottom:1%; margin-top:auto;">
+		
+		<div style="float:right; margin-bottom:1%; margin-top:auto; width:40%; margin-left:auto; margin-right:auto;">
+		<br/>
+		<br/>
 		<a href="ajout_secondaire.php">		
-			<input type="submit" id="second" value="Ajouter un utilisateur secondaire" />
+			<input type="submit" id="seconda" value="Ajouter un utilisateur secondaire" />
 		</a>
+		</div>
 		</div>
 	</article>
 	
--- traitements/ajout_secondaire.php
+++ traitements/ajout_secondaire.php
@@ -12,17 +12,19 @@ catch (Exception $e)
 {
     die('Erreur :' . $e->getMessage());
 }
-
+$id=$_SESSION['ID'];
 $nom = $_POST['name'];
 $mail = $_POST['mail'];
 $type=1;
 $IDprincipal=$_SESSION['ID'];
 $mdp='utilisateur';
 $hash = password_hash($mdp, PASSWORD_BCRYPT);
+$notif="L'utilisateur secondaire " .$nom. " a &eacute;t&eacute; ajout&eacute;" ;
 
 $req = $bdd->prepare('INSERT INTO utilisateur(nom, type, IDprincipal, mail, motdepasse) VALUES(:nom, :type, :id, :mail, :mdp)');
 $result = $req->execute(array(':nom' => $nom, ':type' => $type, ':id' => $IDprincipal, ':mail' => $mail, ':mdp' => $hash));
-
+$req2 = $bdd->prepare('INSERT INTO notification(texte, IDutilisateur) VALUES(:notif, :id)');
+$result2 = $req2->execute(array(':notif' => $notif, ':id' => $id));
 header('location: ../html/tableau_de_bord.php');
 
 ?>
