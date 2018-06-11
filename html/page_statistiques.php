e4fc3e8c126772d2894cf474cbbd03da4a7cd1a2
Naomi <naomicarlotti@yahoo.fr>
2018-06-11 14:49:05 +0200

    modif esthétique de la page stats

--- html/page_statistiques.php
+++ html/page_statistiques.php
@@ -12,10 +12,11 @@
 	       ?>
 </header>
 <article>
-	 <div>
+	 <div id="conteneur2">
 		<form method="post" action="../traitements/traitement_stats.php">
 		
-			<p>  
+			<p>
+			<br>
 			    <h1>
 			    	<abbr title="Les valeurs moyennes de temp&eacute;rature pour des pi&egrave;ces comme le salon, la cuisine, le bureau, etc.. 
 doivent tourner autour des 19 degr&eacute;s C.Pour la salle de bain, la temp&eacute;rature doit &ecirc;tre d'environ 20 degr&eacute;s C.
@@ -23,9 +24,9 @@ Enfin les chambres doivent &ecirc;tre &agrave; une temp&eacute;rature de 16 degr
 un sommeil r&eacute;parateur. 
 Pour les b&eacute;b&eacute;s, 3 degr&eacute;s C de plus sont n&eacute;cessaires du fait de leur fragilit&eacute;.">
 			    		Temp&eacute;rature
-			    	</abbr>.
+			    	</abbr>
 			    </h1>
-			
+				<br>
 			    <label for="nom">
 				Pi&egrave;ces:
 				</label>
@@ -33,16 +34,10 @@ Pour les b&eacute;b&eacute;s, 3 degr&eacute;s C de plus sont n&eacute;cessaires 
 				<select classe="réponse1" name="piece_t">
 					
 					<?php
-					try
-					{
-					$bdd = new PDO('mysql:host=localhost;dbname=bdd_a;charset=utf8','root','',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)); // A modifier lors de l'hebergement
-					}
-					catch (Exception $e)
-					{
-				        die('Erreur : '.$e->getMessage());
-					}
-					$reponse = $bdd->query('SELECT piece.nom FROM piece JOIN maison ON (piece.IDmaison = maison.IDmaison) WHERE  selection = 1'); 
-				
+					include('../modele/config_init.php');
+					$id=$_SESSION['ID']; // on récupère l'id de l'utilisateur connecté
+					$reponse = $bdd->query('SELECT piece.nom FROM piece JOIN maison ON (piece.IDmaison = maison.IDmaison) WHERE  maison.selection = 1 AND maison.IDutilisateur= "'.$id.'"'); 
+					// permet de sélectionner les pièces de la maison sélectionnée uniquement (et de l'utilisateur connecté)
 					while ($donnees = $reponse->fetch())
 					{
 	    			?>
@@ -59,8 +54,8 @@ Pour les b&eacute;b&eacute;s, 3 degr&eacute;s C de plus sont n&eacute;cessaires 
 			</section> 
 					</option> 
 				</select>
-			
-			
+				</br>
+				</br>
 			    <label for="nom">
 				Date:
 				</label>
@@ -70,10 +65,9 @@ Pour les b&eacute;b&eacute;s, 3 degr&eacute;s C de plus sont n&eacute;cessaires 
 				    	<option value="valeur2"selected>Moyenne de ce mois</option>
 				   	<option value="valeur3">Moyenne de cette ann&eacute;e</option>
 				</select>
-			
-</br>
-</br>
-
+				</br>		
+				</br>
+				</br>
 			    <h1>
 			    	<abbr title="Le taux d'humidit&eacute; relative tol&eacute;r&eacute;e est situ&eacute; entre 40 et 60% d'humidit&eacute. 
 Un air trop humide peut provoquer des d&eacute;g&acirc;ts dans la maison et entra&icirc;ner 
@@ -81,9 +75,9 @@ des probl&egrave;mes de sant&eacute;.
 Un air trop sec peut favoriser la pr&eacue;sence de poussi&egrave;re et cr&eacute;er un 
 ass&egrave;chement des muqueuses.">
 					Humidit&eacute;
-			    	</abbr>.
+			    	</abbr>
 			    </h1>
-			
+				</br>
 			    <label for="nom">
 				Pi&egrave;ces:
 				</label>
@@ -91,16 +85,8 @@ ass&egrave;chement des muqueuses.">
 				<select class="réponse1" name="piece_h">
 					
 					<?php
-					try
-					{
-					$bdd = new PDO('mysql:host=localhost;dbname=bdd_a;charset=utf8','root','',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)); // A modifier lors de l'hebergement
-					}
-					catch (Exception $e)
-					{
-				        die('Erreur : '.$e->getMessage());
-					}
-					$reponse = $bdd->query('SELECT piece.nom FROM piece JOIN maison ON (piece.IDmaison = maison.IDmaison) WHERE  selection = 1');
-				
+					$reponse = $bdd->query('SELECT piece.nom FROM piece JOIN maison ON (piece.IDmaison = maison.IDmaison) WHERE maison.selection = 1 AND maison.IDutilisateur= "'.$id.'" ');
+				    // permet de sélectionner les pièces de la maison sélectionnée uniquement (et de l'utilisateur connecté)
 					while ($donnees = $reponse->fetch())
 					{
 	    			?>
@@ -115,8 +101,8 @@ ass&egrave;chement des muqueuses.">
 					?>
 					
 				</select>
-			
-			
+				</br>
+				</br>
 			    <label for="nom">
 				Date:
 				</label>
@@ -126,6 +112,8 @@ ass&egrave;chement des muqueuses.">
 				    	<option value="valeur2"selected>Moyenne de ce mois</option>
 				    	<option value="valeur3">Moyenne de cette ann&eacute;e</option>
 				</select>
+				<br>
+				<br>
 				<div id="formulaire_1">
  							<input type="submit" id="stats" value="Consulter" />
  				</div>
