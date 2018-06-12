c3581e4c03d071ec476bfc8cb2c05fd801d1fd67
alfo10772 <albane.fortier@isep.fr>
2018-06-12 09:25:35 +0200

    modif bouton on off

--- html/exemplepiece.php
+++ html/exemplepiece.php
@@ -39,7 +39,7 @@
 		<div id="conteneurcercle">
 			<?php 
 			$reponse = $bdd->query('SELECT capteur.nom FROM piece JOIN capteur ON (piece.IDpiece = capteur.IDpiece) WHERE capteur.IDpiece = "'.$_SESSION['idpiece'].'"');
-			$reponse2 = $bdd->query('SELECT actionneur.nom, actionneur.etat FROM piece JOIN actionneur ON (piece.IDpiece = actionneur.IDpiece) WHERE actionneur.IDpiece = "'.$_SESSION['idpiece'].'"');
+			$reponse2 = $bdd->query('SELECT actionneur.nom, actionneur.etat, actionneur.IDactionneur FROM piece JOIN actionneur ON (piece.IDpiece = actionneur.IDpiece) WHERE actionneur.IDpiece = "'.$_SESSION['idpiece'].'"');
 			foreach ($reponse->fetchAll() as $donnees)
 			{
 			?>
@@ -53,15 +53,28 @@
 			}
 			foreach ($reponse2->fetchAll() as $donnees2)
 			{
-			?>
+			    $idactionneur= $donnees2['IDactionneur'];
+				$pieces1 = $bdd->query('SELECT piece.nom FROM piece JOIN actionneur ON (piece.IDpiece = actionneur.IDpiece) WHERE actionneur.IDactionneur = "'.$idactionneur.'"');
+			    $piece1 = $pieces1->fetchAll();
+			    ?>
 				<div style="width: 150px;" id="conteneurcompo">
 					<div id="cercle">
-						<label class="switch">
- 						<input type="checkbox" <?php if ($donnees2['etat']== 1) {?> checked <?php }?>> 							<span class="slider round">
-  						</span>
-						</label>
+						<?php if ($donnees2['etat']== 0) {?>
+						<form method="post" action="../traitements/etat_on.php">
+							<input type="submit" name="etat" id="cercleon" value="on" >
+							<input type="hidden" name="source" id="cercleon" value="2">
+							<input type="hidden" name="id" value="<?php echo $donnees2['IDactionneur'];?>">
+						</form>
+						<?php }
+						if ($donnees2['etat']== 1) {?>
+						<form method="post" action="../traitements/etat_off.php">
+							<input type="submit" name="etat" id="cercleoff" value="off" >
+							<input type="hidden" name="source" id="cercleoff" value="2">
+							<input type="hidden" name="id" value="<?php echo $donnees2['IDactionneur'];?>" >
+						</form>
+						<?php }?>
 					</div>
-					<div id="texte"><?php echo $donnees2['nom'];?></div>
+					<div id="texte"><?php echo $donnees2['nom'] . " (" . $piece1[0]['nom'] . ")";?></div>
 				</div>
 						
 			<?php 
--- traitements/etat_on.php
+++ traitements/etat_on.php
@@ -12,13 +12,21 @@ catch (Exception $e)
 }
 
 $id = $_POST['id'];
-
-var_dump($id);
-
+$source = $_POST['source'];
+var_dump($source);
 
 $req = $bdd ->prepare('UPDATE actionneur SET etat=1 WHERE IDactionneur = :id');
 $req-> execute(array(':id' => $id));
 
-header('location: ../html/page_des_composants.php');
+if($source == 1){
+    header('location: ../html/page_des_composants.php');
+}
 
+if($source == 2){
+    header('location: ../html/exemplepiece.php');
+}
+
+if($source == 3){
+   header('location: ../html/tableau_de_bord.php');
+}
 ?>
