c9637e7515e5ee824bfd26900278602ac69ce56e
Ulrich971 <ulrich.firpion@isep.fr>
2018-06-20 16:25:01 +0200

    Intégration électronique et informatique (actionneur)

--- traitements/etat_off.php
+++ traitements/etat_off.php
@@ -12,14 +12,47 @@ $req-> execute(array(':id' => $id));
 //Redirection selon le numéro source de la page
 if($source==1)
 {
+    $ch = curl_init();
+    curl_setopt(
+        $ch,
+        CURLOPT_URL,
+        "http://projets-tomcat.isep.fr:8080/appService?ACTION=COMMAND&TEAM=007A&TRAME=1007A2301002b000b44");
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        echo "Raw Data:<br />";
+        echo("$data");
     header('location: ../html/page_des_composants.php');
 }
 elseif($source==2)
 {
+    $ch = curl_init();
+    curl_setopt(
+        $ch,
+        CURLOPT_URL,
+        "http://projets-tomcat.isep.fr:8080/appService?ACTION=COMMAND&TEAM=007A&TRAME=1007A2301002b000b44");
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        echo "Raw Data:<br />";
+        echo("$data");
     header('location: ../html/exemplepiece.php');
 }
 elseif($source==3)
 {
+    $ch = curl_init();
+    curl_setopt(
+        $ch,
+        CURLOPT_URL,
+        "http://projets-tomcat.isep.fr:8080/appService?ACTION=COMMAND&TEAM=007A&TRAME=1007A2301002b000b44");
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        echo "Raw Data:<br />";
+        echo("$data");
     header('location: ../html/tableau_de_bord.php');
 }
 
--- traitements/etat_on.php
+++ traitements/etat_on.php
@@ -8,17 +8,51 @@ $source = $_POST['source'];     //Récupère la valeur de l'input dont le "name" v
 $req = $bdd ->prepare('UPDATE actionneur SET etat=1 WHERE IDactionneur = :id');     //Change l'état de l'actionneur dans la BDD
 $req-> execute(array(':id' => $id));
 
-
 //Redirection selon le numéro source de la page
 if($source == 1){
+    //include '../html/trame.php';
+    $ch = curl_init();
+    curl_setopt(
+        $ch,
+        CURLOPT_URL,
+        "http://projets-tomcat.isep.fr:8080/appService?ACTION=COMMAND&TEAM=007A&TRAME=1007A2301002a000a44");
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        echo "Raw Data:<br />";
+        echo("$data");
     header('location: ../html/page_des_composants.php');
+    
 }
 
 if($source == 2){
+    $ch = curl_init();
+    curl_setopt(
+        $ch,
+        CURLOPT_URL,
+        "http://projets-tomcat.isep.fr:8080/appService?ACTION=COMMAND&TEAM=007A&TRAME=1007A2301002a000b44");
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        echo "Raw Data:<br />";
+        echo("$data");
     header('location: ../html/exemplepiece.php');
 }
 
 if($source == 3){
+    $ch = curl_init();
+    curl_setopt(
+        $ch,
+        CURLOPT_URL,
+        "http://projets-tomcat.isep.fr:8080/appService?ACTION=COMMAND&TEAM=007A&TRAME=1007A2301002a000b44");
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        echo "Raw Data:<br />";
+        echo("$data");
    header('location: ../html/tableau_de_bord.php');
 }
 ?>
